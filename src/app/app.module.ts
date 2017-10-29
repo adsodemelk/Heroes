12c972395eb8d308aff8ac255782cf283bf26cd9
adsodemelk <nicolas.lopez.carranza@gmail.com>
2017-10-29 20:33:12 +0000

    Add the Angular router to navigate among different components. Create router links to represent navigation menu items. Use router link parameters to navigate to the details of the user-selected hero. Share the HeroService among multiple components. Move HTML and CSS out of the component file and into their own files. Add the uppercase pipe to format data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { AppComponent }  from './app.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
